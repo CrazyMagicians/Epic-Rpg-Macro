e787754f4810126dbbf8649854ab45ceb69c7a5f
CrazyMagicians <67234894+CrazyMagicians@users.noreply.github.com>
2024-10-14 21:39:50 +0530

    Update captcha_bypasser.py
    fixed mistakes

--- captcha_bypasser.py
+++ captcha_bypasser.py
@@ -1,1 +1,1 @@
-print("captcha bypasser is not available for the open source version\nyou can create your own or contact me for the paid version which includes captcha")
+print("captcha bypasser is not available for the open source version\nyou can create your own or contact me for the paid version which includes captcha bypasser")
